9ed1697f773b53da209889d229030f2e08eecaae
Jose Hernandez <josedhg29@gmail.com>
2025-06-13 16:13:42 -0400

    fdkjl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import staticAdapter from "@astrojs/vercel/static";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: "server",
-  adapter: staticAdapter(),
+  site: 'https://astronaut.github.io',
+  base: 'my-repo',
 });
